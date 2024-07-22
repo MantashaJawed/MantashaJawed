8e86c961bdc726de2605190d986babcc0cb1d74d
MantashaJawed <160930207+MantashaJawed@users.noreply.github.com>
2024-07-22 13:45:59 -0700

    Add files via upload

---
+++ public class assignment_operator.py
@@ -0,0 +1,11 @@
+public class assignment_operator
+  {
+   public static void main (string[]args)
+    {
+int number 1,number2;
+  number 1=10;
+  number 2=20;
+  system.out.println("number1="+number1);
+  system.out.println("number2="+number2);
+    }
+  }
